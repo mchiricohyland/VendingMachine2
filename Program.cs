1f37a1281e2542f2fc334ed553cb2325c05ce184
Marie Chirico <Marie.Chirico@hyland.com>
2018-03-07 14:06:19 -0500

    EOD 3/7/18

--- Program.cs
+++ Program.cs
@@ -6,62 +6,115 @@ using System.Threading.Tasks;
 
 namespace Vending_Machine
 {
+
+
+    public class Products
+    {
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+    }
+
     public class Program
     {
-        public class Products
+        public static Products Catalog(int j)
         {
-            public decimal Price { get; set; }
-            public string Type { get; set; }
+            Products[] array = new Products[]
+            {
+            new Products() { Price = 2.00m, Type = "cookies" },
+            new Products() { Price = 1.00m, Type = "chips" },
+            new Products() { Price = 0.50m, Type = "chocolate" },
+            new Products() { Price = 2.75m, Type = "skittles" },
+            new Products() { Price = 2.00m, Type = "pop" },
+            new Products() { Price = 2.25m, Type = "danish" },
+            };
+
+            return array[j];
         }
         public static void Main()
         {
-            decimal currency = 0m, returnedcurrency=0m, change=0m;
-            string type;
+            decimal currency = 0m, change = 0m, total = 0m;
+            string type = "0";
+            Console.WriteLine("***Vending Machine***\n");
+            VendingMachine();//Show All Products
 
-            List<Products> product = new List<Products>
-            {
-                new Products(){Price=2.00m, Type="Cookies"},
-                new Products(){Price=1.00m, Type="Chips"},
-                new Products(){Price=0.50m, Type="Chocolate"},
-                new Products(){Price=2.75m, Type="Skittles"},
-                new Products(){Price=2.00m, Type="Pop"},
-                new Products(){Price=2.25m, Type="Danish"},
-            };
-
-            foreach (Products p in product)
-                Console.WriteLine(p.Type + " " + p.Price);
-
-
-            Console.WriteLine("\nPlease enter the type of snack you want: ");
+            Console.WriteLine("\nPlease enter the type of snack you want: (Enter 99 to indicate end of order)");
             type = Console.ReadLine();
-            Console.WriteLine("\nEnter the amount of money paid: ");
-            currency = decimal.Parse(Console.ReadLine());
 
-            //Console.WriteLine(type + " {0:C2}", currency); //Checking the correct assignment.
+            while (type != "99")
+            {
 
-            returnedcurrency = ProductCatelog(type);
-            Console.WriteLine(returnedcurrency);
+                if (type != "99")
+                {
+                    type.ToLower();
+                    total = SnackTotal(type, total);
+                    Console.WriteLine("Total is {0:C2}", total);
+                }
+                else
+                {
+                    break;
+                }
+                Console.WriteLine("\nPlease enter the type of snack you want: (Enter 99 to indicate end of order)");
+                type = Console.ReadLine();
+            }
+            Console.WriteLine("Total is {0:C2}", total);
 
-            change=Vending(currency, returnedcurrency);
+            try
+            {
+                Console.WriteLine("\nHow many Dollars are you entering? ");
+                currency = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("\nHow many Quarters are you entering? ");
+                currency += (decimal.Parse(Console.ReadLine()) * 0.25m);
+                Console.WriteLine("\nHow many Dimes are you entering? ");
+                currency += (decimal.Parse(Console.ReadLine()) * 0.10m);
+                Console.WriteLine("\nHow many Nickels are you entering? ");
+                currency += (decimal.Parse(Console.ReadLine()) * 0.05m);
+                change = Vending(currency, total);
+                Console.WriteLine("\nYour change is {0:C2}", change);
+            }
 
-            Console.ReadLine();//Stopping the Console Window
+
+            Console.ReadLine();//Stopping the Console Window from closing.
         }
-        public static decimal Vending(decimal currency, decimal snackprice)
+        public static void VendingMachine()
         {
-            decimal dollar = 1m, quarter = .25m, dime = .10m, nickel = .05m;
+            Products[] array2 = new Products[6];
 
-
-        }
-        public static decimal ProductCatelog(string type)
-        {
             for (int i = 0; i < 6; i++)
             {
-                if (StringComparison.OrdinalIgnoreCase(type, List<i>))
-                {
-                    return Products.Price.GetType;
-                };
+                array2[i] = Catalog(i);
             }
-        }
 
+            foreach (Products p in array2)
+                Console.WriteLine(p.Type + " {0:C2}", p.Price);
+        }
+        public static decimal Vending(decimal entry, decimal snackprice)
+        {
+            decimal change = 0m;
+
+            change = entry - snackprice;
+
+            return change;
+
+
+        }
+        public static decimal SnackTotal(string snack, decimal outsidetotal)//Calculates total.
+        {
+            decimal total = outsidetotal;
+            string type;
+            int flag;
+
+            for (int i = 0; i < 6; i++)
+            {
+                type = Catalog(i).Type;
+
+                flag = String.Compare(type, snack);
+
+                if (flag == 0)
+                {
+                    total += Catalog(i).Price;
+                }
+            }
+            return total;
+        }
     }
 }
